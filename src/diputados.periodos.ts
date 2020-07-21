1cfac3c9cfeb0b195eae67e1f384ddc82656930b
jvillane <jvillane@gmail.com>
2020-07-20 21:17:35 -0400

    ADD: deputies legislative periods

--- src/diputados.periodos.ts
+++ src/diputados.periodos.ts
@@ -1,6 +1,6 @@
 import {parseStringPromise} from "xml2js";
 import axios from "axios";
-import {Periodos} from "./diputados.model";
+import {DiputadoPeriodos, PeriodosDiputado} from "./diputados.model";
 import {writeFile} from "fs";
 
 interface LapsesRaw {
@@ -10,22 +10,56 @@ interface LapsesRaw {
   FechaTermino: string[]
 }
 
-async function main() {
-  const lapses: Periodos = {};
+interface PeriodDeputyRaw {
+  Id: string[]
+}
 
-  const response = await axios.get('http://opendata.camara.cl/camaradiputados/WServices/WSLegislativo.asmx/retornarPeriodosLegislativos?');
-  const lapseCollection = await parseStringPromise(response.data);
+interface PeriodRaw {
+  Diputado: PeriodDeputyRaw[]
+}
+
+async function main() {
+  const periodosDiputados: PeriodosDiputado = {};
+  const diputadosPeriodos: DiputadoPeriodos = {};
+
+  const response1 = await axios.get('http://opendata.camara.cl/camaradiputados/WServices/WSLegislativo.asmx/retornarPeriodosLegislativos?');
+  const lapseCollection = await parseStringPromise(response1.data);
   const lapsesRaw: LapsesRaw[] = lapseCollection['PeriodosLegislativosColeccion']['PeriodoLegislativo'];
 
   for (const lapseRaw of lapsesRaw) {
-    lapses[lapseRaw.Id[0]] = {
+    const periodo = {
+      Id: lapseRaw.Id[0],
       Nombre: lapseRaw.Nombre[0],
       Inicio: lapseRaw.FechaInicio[0],
       Termino: lapseRaw.FechaTermino[0],
     }
+    periodosDiputados[periodo.Id] = periodo;
+
+    try {
+      const response2 = await axios.get(`http://opendata.camara.cl/camaradiputados/WServices/WSDiputado.asmx/retornarDiputadosXPeriodo?prmPeriodoId=${periodo.Id}`);
+      const lapseCollection = await parseStringPromise(response2.data);
+      const deputiesPeriods: PeriodRaw[] = lapseCollection['DiputadosPeriodoColeccion']['DiputadoPeriodo'];
+
+      for (const deputy of deputiesPeriods) {
+        const diputadoId = deputy.Diputado[0].Id[0];
+        if (diputadosPeriodos[diputadoId] === undefined) {
+          diputadosPeriodos[diputadoId] = [];
+        }
+        diputadosPeriodos[diputadoId].push(periodo);
+      }
+    } catch (err) {
+      //console.error(err);
+    }
   }
 
-  writeFile("./data/diputados.periodos.json", JSON.stringify(lapses), function (err) {
+
+  writeFile("./data/periodos.diputados.json", JSON.stringify(periodosDiputados), function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  writeFile("./data/diputados.periodos.json", JSON.stringify(diputadosPeriodos), function (err) {
     if (err) {
       console.log(err);
     }
