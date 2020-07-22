b64b1963f5aec7f43e4341bb485caa41b3280f8e
jvillane <jvillane@gmail.com>
2020-07-21 23:31:55 -0400

    FIX: uploading starred voting for timeline

--- src/destacados.votaciones.diputados.ts
+++ src/destacados.votaciones.diputados.ts
@@ -3,13 +3,9 @@ import {VotacionDiputadosDestacada} from "./destacados.model";
 
 export const destacadosVotacionesDiputados: VotacionDiputadosDestacada[] = [
   {
-    Anno: "AAAA",
-    Id: "1313",
-    Resumen: "blablabla"
-  }, {
-    Anno: "AAAA",
-    Id: "1313",
-    Resumen: "blablabla"
+    Anno: "2020",
+    Id: "33634",
+    Resumen: "Votación histórica en virtud del retiro de un 10% de los fondos de la AFP para que las familias chilenas puedan sortear el período de cuarentena establecido por la pandemia del COVID-19"
   }
 ]
 
